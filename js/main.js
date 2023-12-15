39b730fa3d4cee0382d3b37a27d2f4c06567f375
Gabe Ryal <86553565+gaberyal@users.noreply.github.com>
2023-12-15 21:30:18 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ function cutter(str) {
         let secondHalf = str.slice(size/2, size)
         res = secondHalf + firstHalf
     }
-    if (res.length <= 16) {
+    if (res.length >= 16) {
         let half = size/2
         return res.slice(half-8, half+8);
     }
